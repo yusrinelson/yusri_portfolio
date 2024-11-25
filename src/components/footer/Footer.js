3f47ec79cbd9c890151a5297a89af134b61ab597
Yusri Nelson <yusrinelson17@gmail.com>
2024-11-25 15:38:18 +0200

    updated styling

--- src/components/footer/Footer.js
+++ src/components/footer/Footer.js
@@ -2,11 +2,11 @@ import React from 'react'
 
 export default function Footer() {
   return (
-    <div>
+    <div className='mb-7'>
       <div className="w-full border-b-[1px] border-gray-500" />
       <div className="text-center py-2 text-[12px]">
         <p>Copyright &#169; 2024 Yusri Nelson, All Rights .Reserved</p>
       </div>
-      </div>
+    </div>
   )
 }
